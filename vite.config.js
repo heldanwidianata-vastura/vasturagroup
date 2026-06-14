4302120528c4f955b37b18ce5a79d6061d5ab404
Heldan Widianata <heldanwidianata12@gmail.com>
2026-06-15 06:13:01 +0700

    fix: 404 on mobile - fix SPA routing

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 3000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/firestore', 'firebase/storage'],
-        }
-      }
-    }
-  },
-  server: {
-    host: true,
-    port: 5173,
-  },
-  preview: {
-    host: true,
-    port: 4173,
+    chunkSizeWarningLimit: 9000,
   }
 })
